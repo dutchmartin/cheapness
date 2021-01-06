a86673a7696c843d22276668f215a5e92971c66b
dutchmartin <martijn.groeneveldt@gmail.com>
2021-01-06 14:40:33 +0100

    Commit correct recursive equation

--- src/nl/martijngroeneveldt/Main.java
+++ src/nl/martijngroeneveldt/Main.java
@@ -5,55 +5,92 @@ import java.util.Scanner;
 public class Main {
 	static boolean DEBUG = true;
 
-    public static void main(String[] args) {
-	    /*
-	     * Parse input.
-	     */
+	public static void main(String[] args) {
+		/*
+		 * Parse input.
+		 */
 		Scanner scanner = new Scanner(System.in);
 		// The number of products.
-		var n = scanner.nextInt();
+		int n = scanner.nextInt();
 		// The number of dividers.
-	    var k = scanner.nextInt();
-	    // The array with the cost of the products in cents.
-	    var productCosts = new int[n];
-	    for(int i = 0; i < n; i+=1) {
-	    	productCosts[i] = scanner.nextInt();
-	    }
+		int k = scanner.nextInt();
+		// The array with the cost of the products in cents.
+		int[] productCosts = new int[n];
+		for (int i = 0; i < n; i += 1) {
+			productCosts[i] = scanner.nextInt();
+		}
 
-	    /*
-	     * Perform a our algorithm.
-	     */
-	    int totalCost = 0;
-	    int currentCost = productCosts[0];
-	    for (int i = 1; i < productCosts.length; i++) {
-			currentCost += productCosts[i];
-			var lastDigit = lastDigit(currentCost);
-			if(isOptimalDigit(lastDigit) && k > 0 ){
-				// round total
-				var realCost = 0;
-				//if lastDigit >
-				totalCost += 0;
-				currentCost = 0;
-				k--;
-				if(DEBUG)
-					System.out.println("Placed a bar after: " + productCosts[i] + " when i="+ i);
-			}
-	    }
-		totalCost += currentCost;
+		/*
+		 * Perform a our algorithm.
+		 */
 
-	    /*
-	     * Output data.
-	     */
-	    System.out.println(totalCost);
-    }
+		int total = cheapness(k, productCosts, 0, 0);
 
-    static int lastDigit(int x) {
-    	return x % 10;
-    }
 
-    static boolean isOptimalDigit(int x) {
-    	// This can also be x % 5 == 2 || 1, but 4 times a int compare is probably cheaper.
-    	return (x==1 || x==2 || x==6 || x==7);
-	    //return (x==2 || x==7);
-    }
+		/*
+		 * Output data.
+		 */
+		System.out.println(total);
+	}
+
+	static int cheapness(int k, int[] arr, int sum, int i){
+		System.out.println("k: " + k + " sum: " + sum + " i: " + i);
+		if (i == arr.length){ // base case
+			return roundedNumber(sum);
+		}
+		int newSum = sum + arr[i];
+		if(k == 0) {
+			return cheapness(k, arr, newSum, i + 1);
+		}
+		int temp1 = Integer.MAX_VALUE;
+		int temp2 = Integer.MAX_VALUE;
+		if(canBeOptimal(lastDigit(newSum))){
+			// case 1: number can be optimal, but it is not guaranteed that placing the divider after it gives the optimal solution.
+			temp1 = roundedNumber(newSum) + cheapness(k-1,arr, 0, i+1);
+		}
+		{
+			// case 2: Number is not optimal, thus advance in the array.
+			temp2 = cheapness(k, arr, newSum, i+1);
+		}
+		return Math.min(temp1,temp2);
+	}
+
+	static int roundedNumber(int x) {
+		switch (lastDigit(x)) {
+			case 1:
+				return x - 1;
+			case 2:
+				return x - 2;
+			case 3:
+				return x + 2;
+			case 4:
+				return x + 1;
+			case 6:
+				return x - 1;
+			case 7:
+				return x - 2;
+			case 8:
+				return x + 2;
+			case 9:
+				return x + 1;
+			default:
+				return x;
+		}
+	}
+
+	static int lastDigit(int x) {
+		return x % 10;
+	}
+
+	static boolean isOptimal(int x) {
+		return (x == 2 || x == 7);
+	}
+
+	static boolean isSubOptimal(int x) {
+		return (x == 1 || x == 6);
+	}
+
+	static boolean canBeOptimal(int x) {
+		return isSubOptimal(x) || isOptimal(x);
+	}
 }
